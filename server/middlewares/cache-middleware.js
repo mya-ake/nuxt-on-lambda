3c4548d9dbcbef7528d0e780db3dddfa2f7f1281
mya-ake <make.app.ts@gmail.com>
2018-11-19 21:12:20 +0900

    feat: キャッシュミドルウェアを追加

---
+++ server/middlewares/cache-middleware.js
@@ -0,0 +1,20 @@
+/**
+ * キャッシュを行うミドルウェアのサンプル
+ */
+
+const isMyPage = url => {
+  return /^\/my/.test(url)
+}
+
+const cacheMiddleware = (req, res, next) => {
+  if (isMyPage(req.url)) {
+    res.header('Cache-Control', 'no-store, no-cache, max-age=0')
+  } else {
+    res.header('Cache-Control', `max-age=${60}`) // 1分間
+  }
+  next()
+}
+
+module.exports = {
+  cacheMiddleware
+}
