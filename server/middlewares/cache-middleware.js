db8b885e11f7c411d1dc0f5360ad198ad231f32c
mya-ake <make.app.ts@gmail.com>
2019-04-07 14:45:34 +0900

    refactor: Update cache control

--- server/middlewares/cache-middleware.js
+++ server/middlewares/cache-middleware.js
@@ -8,7 +8,7 @@ const isMyPage = url => {
 
 const cacheMiddleware = (req, res, next) => {
   if (isMyPage(req.url)) {
-    res.header('Cache-Control', 'no-store, no-cache, max-age=0');
+    res.header('Cache-Control', 'no-store');
   } else {
     res.header('Cache-Control', `max-age=${60}`); // 1分間
   }
